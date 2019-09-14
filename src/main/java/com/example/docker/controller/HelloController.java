9a4142b62d1cc1241108a2b1abcc05ffc836e3f9
Ana Lucatti <analucatti23@gmail.com>
2019-09-14 09:00:18 -0300

    Add hello controller endpoint

---
+++ src/main/java/com/example/docker/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Welcome to DevOps culture!!!";
+    }
+}
